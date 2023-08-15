30aec8e37b2b567c5261d1ab43136af29c628405
miguelangelacosta <113574101+miguelangelacosta@users.noreply.github.com>
2023-08-15 16:41:36 -0500

    01

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+import Section from './Section';
+import Statistics from './Statistics';
+
+function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (option) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback === 0 ? 0 : Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <div>
+      <Section title="Leave your feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+      {totalFeedback > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FeedbackOptions.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div>
+      {options.map((option) => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
+
